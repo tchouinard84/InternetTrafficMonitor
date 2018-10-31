749757559eabd3761f6048637fac5f6d6120336d
tchouina <timothy.chouinard@emp-corp.com>
2018-10-31 08:50:29 -0400

    Added a new .NET Core console app

---
+++ InternetTrafficMonitor/InternetMonitorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InternetMonitorApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
